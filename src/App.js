5b8e809c0ef6c7599c821e45281de8e2a66672eb
Li YungHao <liyunghao@gmail.com>
2018-06-28 20:23:35 +0800

    NavBar fixed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import Ride from './Pages/Ride'
 import Chatting from './Pages/Chatting'
 import Setting from './Pages/Setting'
+import NavBar from './Components/NavBar'
 
 
 const Notfound = () => (<div> ERROR 404 </div>)
@@ -16,13 +17,18 @@ class App extends Component {
     return (
       <div className = 'App'>
         <BrowserRouter>
-          <Switch>
-            <Route exact path = '/' component = { Home }/>
-            <Route path = '/Ride' component = { Ride }/>
-            {/* <Route path = '/Chatting' component = { Chatting }/> */}
-            {/* <Route path = '/Setting' component = { Setting }/>  */}
-            <Route component = { Notfound }/>
-          </Switch>
+          <div>
+            <Route path = '/Ride' component = {NavBar}/>
+            <Route path = '/Chatting' component = {NavBar}/>
+            <Route path = '/Setting' component = {NavBar}/>
+            <Switch>
+              <Route exact path = '/' component = { Home }/>
+              <Route path = '/Ride' component = { Ride }/>
+              {/* <Route path = '/Chatting' component = { Chatting }/> */}
+              {/* <Route path = '/Setting' component = { Setting }/>  */}
+              <Route component = { Notfound }/>
+            </Switch>
+          </div>
         </BrowserRouter>
 
       </div>
