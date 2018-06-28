71a026eccb323b651b59791b4dc12f8d584883a3
Li YungHao <liyunghao@gmail.com>
2018-06-28 16:03:00 +0800

    Route and Pages added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home'
+import Ride from './Pages/Ride'
+import Chatting from './Pages/Chatting'
+import Setting from './Pages/Setting'
 
+const Notfound = () => (<div> ERROR 404 </div>)
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello world
+      <div className = 'App'>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path = '/' component = { Home }/>
+            <Route path = '/Ride' component = { Ride }/>
+            <Route path = '/Chatting' component = { Chatting }/>
+            <Route path = '/Setting' component = { Setting }/> 
+            <Route component = { Notfound }/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
