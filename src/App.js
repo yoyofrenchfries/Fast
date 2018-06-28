509646953ff05a16dcef410b8730c9c480022d6b
Li YungHao <liyunghao@gmail.com>
2018-06-28 16:15:58 +0800

    debug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
-import Home from './Pages/Home'
+// import Home from './Pages/Home'
 import Ride from './Pages/Ride'
 import Chatting from './Pages/Chatting'
 import Setting from './Pages/Setting'
 
 const Notfound = () => (<div> ERROR 404 </div>)
+const Home = () => (<div> Home Page </div>)
 class App extends Component {
   render() {
     return (
