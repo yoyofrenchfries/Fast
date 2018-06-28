9c22245e3dee20ac3d3fb0694fe876b3e3430049
yoyofrenchfries <yaon12100@gmail.com>
2018-06-28 18:44:36 +0800

    resolve merge conflict done

--- src/Components/CardItem.js
+++ src/Components/CardItem.js
@@ -6,11 +6,11 @@ class CardItem extends React.Component{
     return(
       <div className = 'Container'>
         <div>
-          <div class="card" >
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+          <div className="card" >
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           </div>
         </div>
