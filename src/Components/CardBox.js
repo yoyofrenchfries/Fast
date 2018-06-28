b0bd4bbbaf7e2fa67088ad1eb6d3bb7e62616b2e
Li YungHao <liyunghao@gmail.com>
2018-06-28 16:28:54 +0800

    c

--- src/Components/CardBox.js
+++ src/Components/CardBox.js
@@ -1,73 +1,78 @@
 import React from 'react';
 
 class CardBox extends React.Component {
-
-	const fake_data = [
-		{
-		    "status": "succeed",
-		    "data": [
-		    {
-		    "id": 1,
-		    "title": "test",
-		    "time_start": null,
-		    "time_end": null,
-		    "description": null,
-		    "from": null,
-		    "to": null,
-		    "vehicle": null,
-		    "num_passenger_max": null,
-		    "created_at": "2018-06-28T07:27:30.265Z",
-		    "updated_at": "2018-06-28T07:27:30.265Z"    
-		    }
-		    ]
-		},
-		{
-		    "status": "succeed",
-		    "data": [
-		    {
-		    "id": 2,
-		    "title": "test",
-		    "time_start": null,
-		    "time_end": null,
-		    "description": null,
-		    "from": null,
-		    "to": null,
-		    "vehicle": null,
-		    "num_passenger_max": null,
-		    "created_at": "2018-06-28T07:27:30.265Z",
-		    "updated_at": "2018-06-28T07:27:30.265Z"    
-		    }
-		    ]
-		},
-		{
-		    "status": "succeed",
-		    "data": [
-		    {
-		    "id": 3,
-		    "title": "test",
-		    "time_start": null,
-		    "time_end": null,
-		    "description": null,
-		    "from": null,
-		    "to": null,
-		    "vehicle": null,
-		    "num_passenger_max": null,
-		    "created_at": "2018-06-28T07:27:30.265Z",
-		    "updated_at": "2018-06-28T07:27:30.265Z"    
-		    }
-		    ]
+	constructor(props){
+		super(props)
+		this.state = {
+			fake_data : [
+				{
+						"status": "succeed",
+						"data": [
+						{
+						"id": 1,
+						"title": "test",
+						"time_start": null,
+						"time_end": null,
+						"description": null,
+						"from": null,
+						"to": null,
+						"vehicle": null,
+						"num_passenger_max": null,
+						"created_at": "2018-06-28T07:27:30.265Z",
+						"updated_at": "2018-06-28T07:27:30.265Z"    
+						}
+						]
+				},
+				{
+						"status": "succeed",
+						"data": [
+						{
+						"id": 2,
+						"title": "test",
+						"time_start": null,
+						"time_end": null,
+						"description": null,
+						"from": null,
+						"to": null,
+						"vehicle": null,
+						"num_passenger_max": null,
+						"created_at": "2018-06-28T07:27:30.265Z",
+						"updated_at": "2018-06-28T07:27:30.265Z"    
+						}
+						]
+				},
+				{
+						"status": "succeed",
+						"data": [
+						{
+						"id": 3,
+						"title": "test",
+						"time_start": null,
+						"time_end": null,
+						"description": null,
+						"from": null,
+						"to": null,
+						"vehicle": null,
+						"num_passenger_max": null,
+						"created_at": "2018-06-28T07:27:30.265Z",
+						"updated_at": "2018-06-28T07:27:30.265Z"    
+						}
+						]
+				}
+			]
 		}
-	]
+	}
+	
 
 	render(){
 		return (
 			<div>
 				{
-						fake_data.length>0?
-							fake_data.map( (data) =>
-								<CardItem key = {data.id}/>
-							)
-							: <div></div>
+					this.fake_data.length>0?
+						fake_data.map( (data) =>
+							<CardItem key = {data.id}/>
+						)
+						: <div></div>
 				}
 			</div>
 		)
