2ecf092012f807f4926926b6add89295e131c836
SwallowLee <girlannie123@gmail.com>
2018-06-28 16:13:35 +0800

    add cardbox.js

---
+++ src/Components/CardBox.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+class CardBox extends React.Component {
+
+	const fake_data = [
+		{
+		    "status": "succeed",
+		    "data": [
+		    {
+		    "id": 1,
+		    "title": "test",
+		    "time_start": null,
+		    "time_end": null,
+		    "description": null,
+		    "from": null,
+		    "to": null,
+		    "vehicle": null,
+		    "num_passenger_max": null,
+		    "created_at": "2018-06-28T07:27:30.265Z",
+		    "updated_at": "2018-06-28T07:27:30.265Z"    
+		    }
+		    ]
+		},
+		{
+		    "status": "succeed",
+		    "data": [
+		    {
+		    "id": 2,
+		    "title": "test",
+		    "time_start": null,
+		    "time_end": null,
+		    "description": null,
+		    "from": null,
+		    "to": null,
+		    "vehicle": null,
+		    "num_passenger_max": null,
+		    "created_at": "2018-06-28T07:27:30.265Z",
+		    "updated_at": "2018-06-28T07:27:30.265Z"    
+		    }
+		    ]
+		},
+		{
+		    "status": "succeed",
+		    "data": [
+		    {
+		    "id": 3,
+		    "title": "test",
+		    "time_start": null,
+		    "time_end": null,
+		    "description": null,
+		    "from": null,
+		    "to": null,
+		    "vehicle": null,
+		    "num_passenger_max": null,
+		    "created_at": "2018-06-28T07:27:30.265Z",
+		    "updated_at": "2018-06-28T07:27:30.265Z"    
+		    }
+		    ]
+		}
+	]
+
+	render(){
+		return (
+			<div>
+				{
+						fake_data.length>0?
+							fake_data.map( (data) =>
+								<CardItem key = {data.id}/>
+							)
+							: <div></div>
+				}
+			</div>
+		)
+	}
+}
+
+export default CardBox;
